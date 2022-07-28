dda583cf85d1ef446ccf59345f1dc2605dfb6445
iDEaXANA <bmbenkada@gmail.com>
2022-07-28 14:45:21 +0100

    Invoking functions multiple times

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 'use strict'
 // document.getElementById("count-el").innerText = 5
 
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
+let lapsCompleted = 0
 
 function sum() {
-    let lapTotal = lap1 + lap2 + lap3
-    console.log(lapTotal)
+    lapsCompleted = lapsCompleted + 1
 }
 sum()
+sum()
+sum() // Renaming to Increment would've helped me notice what was going on!
+console.log(lapsCompleted)
+
+
+
+
