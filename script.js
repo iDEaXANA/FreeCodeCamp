9fe619ed7fec8ace4144c2fae2cb5aa752ec52bc
iDEaXANA <bmbenkada@gmail.com>
2022-07-28 14:38:34 +0100

    Further manipulation of functions

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict'
 // document.getElementById("count-el").innerText = 5
 
-count = 0
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
 
-function fourtytwo() {
-    console.log(42)
+function sum() {
+    let lapTotal = lap1 + lap2 + lap3
+    console.log(lapTotal)
 }
-
-fourtytwo()
+sum()
