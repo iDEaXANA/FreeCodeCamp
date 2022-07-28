4347b987989355f080a369510158d26354bd2440
iDEaXANA <bmbenkada@gmail.com>
2022-07-28 14:26:36 +0100

    Creating and using functions!

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 'use strict'
 // document.getElementById("count-el").innerText = 5
 
-let count = 0
+count = 0
 
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-bonusPoints = bonusPoints - 75
-bonusPoints = bonusPoints + 45
+function fourtytwo() {
+    console.log(42)
+}
 
-console.log(bonusPoints)
+fourtytwo()
