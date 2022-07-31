42be80f912c7f21ce84bef5ae7860d2e9e684189
iDEaXANA <bmbenkada@gmail.com>
2022-07-31 13:43:04 +0100

    Created save button which prints out previous count result

--- script.js
+++ script.js
@@ -2,19 +2,39 @@
 // document.getElementById("count-el").innerText = 5
 
 let countEl = document.getElementById("count-el")
-
-console.log(countEl)
-
 let count = 0
 
 function increment() {
-    console.log("clicked")
-    count = count + 1
-    console.log(count)
+    count += 1
     countEl.innerText = count
 }
 increment()
 
+let saveEl = document.getElementById("save-el")
 
+function save() {
+    let countStr = count + "--"
+    saveEl.innerText += countStr // Got stuck on this for 15 minutes. .innertext =/= .innerText....
+}
+save()
+// let username = "Bilal"
+// let message = "You have three new notifications"
+// let messageToUser = message + ", " + username + "!"
+
+// console.log(messageToUser)
+
+// let name = "Bilal"
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + name 
+
+// console.log(myGreeting)
+
+
+// let welcomeEl = document.getElementById("welcome-el")
+// let name = "Bilal"
+// let greeting = "Hi, my name is "
+
+// welcomeEl.innerText = greeting + name
+// welcomeEl.innerText += "👋"
 
 
